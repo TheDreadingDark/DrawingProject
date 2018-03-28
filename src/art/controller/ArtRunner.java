86da29ecab617fd3fc1b75518052aba18bbda84c
TheDreadingDark <31518923+TheDreadingDark@users.noreply.github.com>
2018-03-28 12:37:14 -0600

    Added ArtPanel and code, also added .gitignore.

---
+++ src/art/controller/ArtRunner.java
@@ -0,0 +1,12 @@
+package art.controller;
+
+import art.controller.ArtController;
+
+public class ArtRunner
+{
+	public static void main(String [] args)
+	{
+		ArtController app = new ArtController();
+		app.start();
+	}
+}
